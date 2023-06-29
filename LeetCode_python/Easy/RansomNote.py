2ed956ce94a09db8115838a21aca655ea41b42be
maxshchurr <mxm.shchur@gmail.com>
2023-06-29 23:28:33 +0300

    added comments for python solution + java tasks section

--- LeetCode_python/Easy/RansomNote.py
+++ LeetCode_python/Easy/RansomNote.py
@@ -1,3 +1,26 @@
+"""
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+"""
+
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         for i in ransomNote:
