1038adf4d766b68e6337e58a5884c15bfbc1eee9
maxshchurr <mxm.shchur@gmail.com>
2023-06-30 14:44:39 +0300

    new task

---
+++ LeetCode_python/Medium/WordBreak.py
@@ -0,0 +1,40 @@
+"""
+
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+
+"""
+
+
+# was hard task to understand even after looking for solutions in solutions chapter
+class Solution:
+    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                # checking substrings to match one of the elems in wordDict
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+
+        return dp[-1]
