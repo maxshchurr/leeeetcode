5c97c7d8be247df583a1485c080651fd07a3d390
maxshchurr <mxm.shchur@gmail.com>
2023-08-24 13:47:53 +0300

    python codewars

---
+++ Codewars/7kyu/Vowel Count.py
@@ -0,0 +1,14 @@
+"""
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+"""
+
+
+def get_count(sentence):
+    vowels = 'aeiou'
+    return sum(vowel in vowels for vowel in sentence)
