909afe2d807d5308d990be2bbdfa48da2b6d02d2
maxshchurr <mxm.shchur@gmail.com>
2023-09-08 23:17:49 +0300

    python

---
+++ Codewars/6kyu/Simple Fun #305: Typist.py
@@ -0,0 +1,51 @@
+"""
+
+Task
+John is a typist. He has a habit of typing: he never use the Shift key to switch case, just using only Caps Lock.
+
+Given a string s. Your task is to count how many times the keyboard has been tapped by John.
+
+You can assume that, at the beginning the Caps Lock light is not lit.
+
+Input/Output
+[input] string s
+
+A non-empty string. It contains only English letters(uppercase or lowercase).
+
+1 ≤ s.length ≤ 10000
+
+[output] an integer
+
+The number of times John tapped the keyboard.
+
+Example
+For s = "a", the output should be 1.
+
+John hit button a.
+
+For s = "aa", the output should be 2.
+
+John hit button a, a.
+
+For s = "A", the output should be 2.
+
+John hit button Caps Lock, A.
+
+For s = "Aa", the output should be 4.
+
+John hit button Caps Lock, A, Caps Lock, a.
+
+"""
+
+def typist(s):
+    cnt = 1 if s[0].islower() else 2
+
+    for i in range(1, len(s)):
+        if s[i].islower() and s[i-1].isupper():
+            cnt += 2
+        elif s[i].isupper() and s[i-1].islower():
+            cnt += 2
+        else:
+            cnt += 1
+
+    return cnt
