c8de59ee2127e76a5a516d7c5bb52d92157b9761
maxshchurr <mxm.shchur@gmail.com>
2023-09-11 22:00:25 +0300

    python leeetcodeee

--- LeetCode_python/Easy/Counting Bits.py
+++ LeetCode_python/Easy/Counting Bits.py
@@ -58,9 +58,3 @@ class Solution1:
         for i in range(1, n+1):
             counter.append(counter[i >> 1] + i % 2)
         return counter
-
-
-print(Solution1().countBits(10))
-
-
-print(7 >> 2)
