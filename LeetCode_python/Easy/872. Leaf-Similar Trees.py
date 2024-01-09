2199991d0169263c00e823068a495308386de896
maxshchurr <mxm.shchur@gmail.com>
2024-01-09 18:17:48 +0200

    python easy

---
+++ LeetCode_python/Easy/872. Leaf-Similar Trees.py
@@ -0,0 +1,68 @@
+"""
+
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+
+Example 1:
+
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+Example 2:
+
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
+
+"""
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
+
+        def dfs(root):
+            if not root:
+                return []
+
+            if not root.left and not root.right:
+                return [root.val]
+
+            return dfs(root.left) + dfs(root.right)
+
+        return dfs(root1) == dfs(root2)
+
+
+# same idea
+class Solution1:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def collect_leaf_values(root, leaf_values):
+            if not root:
+                return
+            if not root.left and not root.right:
+                leaf_values.append(root.val)
+            collect_leaf_values(root.left, leaf_values)
+            collect_leaf_values(root.right, leaf_values)
+
+        leaf_values1 = []
+        leaf_values2 = []
+
+        collect_leaf_values(root1, leaf_values1)
+        collect_leaf_values(root2, leaf_values2)
+
+        return leaf_values1 == leaf_values2
