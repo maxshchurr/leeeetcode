00f0ae6c676029dc1a15e2e027336ab108821acc
maxshchurr <mxm.shchur@gmail.com>
2024-01-20 03:55:19 +0200

    python mock

---
+++ LeetCode_python/Leetcode_MockAssessment/mock19.py
@@ -0,0 +1,41 @@
+"""
+
+Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s, split it into some number of substrings such that:
+
+Each substring is balanced.
+Return the maximum number of balanced strings you can obtain.
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", each substring contains same number of 'L' and 'R'.
+Note that s cannot be split into "RL", "RR", "RL", "LR", "LL", because the 2nd and 5th substrings are not balanced.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+
+"""
+
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        res = 0
+        curr = ''
+
+        for i in range(len(s)):
+            curr += s[i]
+            if curr.count('R') == curr.count('L') and curr.count('R') != 0 and curr.count('L') != 0:
+                res += 1
+                curr = ''
+
+        return res
